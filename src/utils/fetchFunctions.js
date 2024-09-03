8de07982936cb7ec9450963775446e7b05b3079d
Bruno H <riquebruno@yahoo.com.br>
2024-09-03 17:58:35 -0300

    feat: adiciona funções para manipular API do ML (Item, Categoria, Lista...)

---
+++ src/utils/fetchFunctions.js
@@ -0,0 +1,26 @@
+export const fetchProduct = async (produto) => {
+  if (!produto) throw new Error('ID não informado');
+  const response = await fetch(`https://api.mercadolibre.com/items/${produto}`);
+  const data = await response.json();
+  return data;
+};
+
+export const fetchProductsList = async (item) => {
+  if (!item) throw new Error('Termo de busca não informado');
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
+  const { results } = await response.json();
+  return results;
+};
+
+export const fetchcategories = async () => {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/categories`);
+  const data = await response.json();
+  return data;
+};
+
+export const fetchProductsByCategoriesList = async (category) => {
+  if (!category) throw new Error('Termo de busca não informado');
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${category}`);
+  const { results } = await response.json();
+  return results;
+};
