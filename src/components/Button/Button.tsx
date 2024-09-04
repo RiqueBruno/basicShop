04d0e89f9ef7d8e456e8fe6eaa37912e8ad9a920
Bruno H <riquebruno@yahoo.com.br>
2024-09-04 20:02:14 -0300

    feat: adiciona twMerge ao button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,25 @@
-import React  from 'react'
+import React from 'react';
+import { twMerge } from 'tailwind-merge'
 import { buttonProps } from '../../types/Button'
 
-function Button({ text, onClick, ...props }: buttonProps) {
+function Button({ className, text, onClick, ...props }: buttonProps) {
   return (
     //caso precise de mais botões pode usar o twMerge que ele ajuda na questão
-    //da classes do tailwind
-    <button onClick={onClick} {...props}>{text}</button>
+    //da classes do tailwind quano se quer dois estilos diferentes ou mais
+    //npm i tailwind-merge
+    //import { twMerge } from 'tailwind-merge'
+    //className={twMerge('bg-blue-500', 'text-white')}
+    //className={twMerge('estilo padrao', 'estilo quando hover por props no className')}
+    <button
+      className={twMerge(className)}
+      onClick={onClick}
+      {...props}
+    >
+      {
+        text
+      }
+    </button>
   )
 }
 
-export default Button
+export default Button;
