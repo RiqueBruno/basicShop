1129bfc845777db0a43e01e2f97494673091a610
Bruno H <riquebruno@yahoo.com.br>
2024-09-05 19:16:33 -0300

    refactor: muda cartFunctions e fetchFunctions de js para ts

--- src/utils/cartFunctions.ts
+++ src/utils/cartFunctions.ts
@@ -1,11 +1,11 @@
- //Função que retorna todos os itens do carrinho salvos no localStorage.
+//Função que retorna todos os itens do carrinho salvos no localStorage.
 export const getSavedCartIDs = () => {
   const cartProducts = localStorage.getItem('cartProducts');
   return cartProducts ? JSON.parse(cartProducts) : [];
 };
 
- //Função que adiciona um product ao carrinho.
-export const saveCartID = (id) => {
+//Função que adiciona um product ao carrinho.
+export const saveCartID = (id: object) => {
   if (!id) throw new Error('Você deve fornecer um ID');
 
   const cartProducts = getSavedCartIDs();
@@ -13,8 +13,8 @@ export const saveCartID = (id) => {
   localStorage.setItem('cartProducts', JSON.stringify(newCartProducts));
 };
 
- //Função que remove um product do carrinho.
-export const removeCartID = (id) => {
+//Função que remove um product do carrinho.
+export const removeCartID = (id: number) => {
   if (!id) throw new Error('Você deve fornecer um ID');
 
   const cartProducts = [...getSavedCartIDs()];
--- src/utils/fetchFunctions.ts
+++ src/utils/fetchFunctions.ts
@@ -1,26 +1,32 @@
-export const fetchProduct = async (produto) => {
+export const fetchProduct = async (produto: string) => {
   if (!produto) throw new Error('ID não informado');
   const response = await fetch(`https://api.mercadolibre.com/items/${produto}`);
   const data = await response.json();
   return data;
 };
 
-export const fetchProductsList = async (item) => {
+export const fetchProductsList = async (item: string) => {
   if (!item) throw new Error('Termo de busca não informado');
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
+  const response = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?q=${item}`,
+  );
   const { results } = await response.json();
   return results;
 };
 
 export const fetchcategories = async () => {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/categories`);
+  const response = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/categories`,
+  );
   const data = await response.json();
   return data;
 };
 
-export const fetchProductsByCategoriesList = async (category) => {
+export const fetchProductsByCategoriesList = async (category: string) => {
   if (!category) throw new Error('Termo de busca não informado');
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${category}`);
+  const response = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${category}`,
+  );
   const { results } = await response.json();
   return results;
 };
