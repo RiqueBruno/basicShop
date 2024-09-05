006c51934754b9f48308f56da8db6d013f550366
Bruno H <riquebruno@yahoo.com.br>
2024-09-05 19:05:27 -0300

    feat: adiciona função onClick ao  Cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import cart from '../../assets/icons/cart.svg';
 import Button from '../Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Cart() {
+    const navigate = useNavigate();
+    
+
+    const onClick = () => {
+        navigate('/cart');
+    };
+
   return (
-    <Button text='' onClick={() => {}}>
+    <Button text='' onClick={onClick}>
         <p></p>
         <img src={ cart } alt="Carrinho de compras" />
     </Button>
