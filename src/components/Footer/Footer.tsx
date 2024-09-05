1a21e2e6f13a1f49ca050bb11c288249f36dd3b2
Bruno H <riquebruno@yahoo.com.br>
2024-09-04 21:29:28 -0300

    feat: adiciona componente footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-yellow-400 w-full h-10 mt-auto flex justify-center items-center text-xs">
+      <p className="text-blue-950 text-center">
+        Copyright © 2024 Bruno H. Cardoso Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+}
