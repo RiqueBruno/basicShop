19a10f027c1e7005f5ef30a9a088e01fbedd125a
Bruno H <riquebruno@yahoo.com.br>
2024-09-05 19:26:47 -0300

    feat: adiciona interface de Item

---
+++ src/interfaces/Item.ts
@@ -0,0 +1,10 @@
+export interface Item {
+    id: string;
+    title: string;
+    price: number;
+    initial_quantity: number;
+    thumbnail: string;
+    pictures: object[];
+    descriptions: object[];
+    warranty: string;
+}
