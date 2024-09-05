47a5a0305f18116900e8e1f04d4eb55366236336
Bruno H <riquebruno@yahoo.com.br>
2024-09-04 21:21:54 -0300

    style: reseta o css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
-import Content from './components/Content'
+import Content from './components/Content';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
-    <div>
+    <div className='bg-orange-500 flex flex-col min-h-screen'>
+      <Header />
       <Content />
+      <Footer />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -6,8 +6,6 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <StrictMode>
       <App />
-    </StrictMode>,
   </BrowserRouter>
 )
