01d586c651f504dac67ed3f96351ff26ed4b5e01
Bruno H <riquebruno@yahoo.com.br>
2024-09-05 22:57:02 -0300

    style: arruma style do componente Cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from 'react';
 import cart from '../../assets/icons/cart.svg';
 import Button from '../Button/Button';
 import { useNavigate } from 'react-router-dom';
+import { getSavedCartIDs } from '../../utils/cartFunctions';
 
 export default function Cart() {
-    const navigate = useNavigate();
-    
+  const navigate = useNavigate();
+  const numberOfItems: number = getSavedCartIDs().length;
 
-    const onClick = () => {
-        navigate('/cart');
-    };
+  const onClick = () => {
+    navigate('/cart');
+  };
 
   return (
-    <Button text='' onClick={onClick}>
-        <p></p>
-        <img src={ cart } alt="Carrinho de compras" />
+    <Button className="relative flex items-center justify-center h-8 w-8" text="" onClick={onClick}>
+      <p className="absolute text-red-800 text-sm bottom-5 right-3">{numberOfItems}</p>
+      <img src={cart} alt="Carrinho de compras" className="absolute top-1" />
     </Button>
-  )
+  );
 }
