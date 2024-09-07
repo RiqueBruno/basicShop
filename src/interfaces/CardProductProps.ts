d903094d39b433074ba7ad1019dff0dd159a99e2
Bruno H <riquebruno@yahoo.com.br>
2024-09-07 10:31:18 -0300

    feat: adiciona interface cardProductProps

---
+++ src/interfaces/CardProductProps.ts
@@ -0,0 +1,11 @@
+export interface CardProductProps {
+    index: number;
+    id: string;
+    title: string;
+    price: number;
+    initial_quantity: number;
+    thumbnail: string;
+    pictures: object[];
+    descriptions: object[];
+    warranty: string;
+}
