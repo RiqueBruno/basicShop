30b52799158fde494ff3df34c69b2e06161849b5
Bruno H <riquebruno@yahoo.com.br>
2024-09-07 09:43:47 -0300

    feat: adiciona home ao content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home/Home'
 
 export default function Content() {
   return (
     <Routes>
-      <Route path='/' Component={() => (<h1>Home</h1>)}></Route>
+      <Route path='/' Component={Home}></Route>
       <Route path='/search?product=:id' Component={() => (<h1>search Product</h1>)}></Route>
       <Route path='/search?category=:id' Component={() => (<h1>search Category</h1>)}></Route>
       <Route path='/cart' Component={() => (<h1>Cart</h1>)}></Route>
