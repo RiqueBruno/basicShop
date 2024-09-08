b3a6e72d3a133b10bb19d0c7764595b4e71dd187
Bruno H <riquebruno@yahoo.com.br>
2024-09-08 16:24:47 -0300

    feat: adiciona page Search

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home/Home'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Search from '../pages/Search/Search';
 
 export default function Content() {
   return (
     <Routes>
-      <Route path='/' Component={Home}></Route>
-      <Route path='/search?product=:id' Component={() => (<h1>search Product</h1>)}></Route>
-      <Route path='/search?category=:id' Component={() => (<h1>search Category</h1>)}></Route>
-      <Route path='/cart' Component={() => (<h1>Cart</h1>)}></Route>
-      <Route path='/favorite' Component={() => (<h1>favorite</h1>)}></Route>
-      <Route path='/product/:id' Component={() => (<h1>Product</h1>)}></Route>
-      <Route path='*'>Page Not Found</Route>
+      <Route path="/" Component={Home}></Route>
+      <Route path="/search?product=:id" Component={Search}></Route>
+      <Route path="/search?category=:id" Component={Search}></Route>
+      <Route path="/cart" Component={() => <h1>Cart</h1>}></Route>
+      <Route path="/favorite" Component={() => <h1>favorite</h1>}></Route>
+      <Route path="/product/:id" Component={() => <h1>Product</h1>}></Route>
+      <Route path="*">Page Not Found</Route>
     </Routes>
-  )
+  );
 }
---
+++ src/pages/Search/Search.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Search() {
+  //pegar a url com window.location.search
+  //dividir com split('=')
+  //verificar se o primeiro item é igual a 'product' ou 'category'
+  //usar as funções utils para pegar ps produtos de acordo com o id buscado
+  //verificar filtros
+  return <div>Search</div>;
+}
