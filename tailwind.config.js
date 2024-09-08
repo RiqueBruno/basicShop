b0f3c2fc6b2627be1b89b929e31ed073149262d7
Bruno H <riquebruno@yahoo.com.br>
2024-09-08 15:02:32 -0300

    refactor: atualiza package.json e tailwind.config.js com animações slideIn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(100%)' },
+          '50%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-400px)' },
+        },
+        slideInDesktop: {
+          '0%': { transform: 'translateX(100%)' },
+          '50%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        slideIn: 'slideIn 5s ease-out infinite',
+        slideInDesktop: 'slideInDesktop 4.9s ease-out infinite',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 };
