e394031c34e5a6e6a70a58f29cfaeeff7a4817f1
Bruno H <riquebruno@yahoo.com.br>
2024-09-09 18:43:06 -0300

    feat: adiciona page favorito

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Search from '../pages/Search/Search';
 import Product from '../pages/Product/Product';
+import Favorite from '../pages/Favorite/Favorite';
 
 export default function Content() {
   return (
@@ -10,7 +11,7 @@ export default function Content() {
       <Route path="/" Component={Home}></Route>
       <Route path="/search" Component={Search}></Route>
       <Route path="/cart" Component={() => <h1>Cart</h1>}></Route>
-      <Route path="/favorite" Component={() => <h1>favorite</h1>}></Route>
+      <Route path="/favorite" Component={Favorite}></Route>
       <Route path="/product/:id" Component={Product}></Route>
       <Route path="*">Page Not Found</Route>
     </Routes>
---
+++ src/pages/Favorite/Favorite.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Favorite() {
+  //pegar favoritos do localstorage
+  //mostrar favoritos
+  return <div>Favorite</div>;
+}
