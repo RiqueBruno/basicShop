e5d6bcca1abb7e50b1216949f9ed4992ff001eea
Bruno H <riquebruno@yahoo.com.br>
2024-09-09 22:05:45 -0300

    feat: adiciona page Cart

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,13 +4,14 @@ import Home from '../pages/Home/Home';
 import Search from '../pages/Search/Search';
 import Product from '../pages/Product/Product';
 import Favorite from '../pages/Favorite/Favorite';
+import Cart from '../pages/Cart/Cart';
 
 export default function Content() {
   return (
     <Routes>
       <Route path="/" Component={Home}></Route>
       <Route path="/search" Component={Search}></Route>
-      <Route path="/cart" Component={() => <h1>Cart</h1>}></Route>
+      <Route path="/cart" Component={Cart}></Route>
       <Route path="/favorite" Component={Favorite}></Route>
       <Route path="/product/:id" Component={Product}></Route>
       <Route path="*">Page Not Found</Route>
