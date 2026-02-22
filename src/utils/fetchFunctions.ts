d675181e1a4d12b12045af4441c7bbe07682abde
Bruno <riquebruno@yahoo.com.br>
2026-02-22 20:24:09 -0300

    fix: api ML connection

--- src/utils/fetchFunctions.ts
+++ src/utils/fetchFunctions.ts
@@ -1,24 +1,40 @@
+const TOKEN = import.meta.env.VITE_ML_ACCESS_TOKEN;
+
+const headers = {
+  'Authorization': `Bearer ${TOKEN}`,
+  'Content-Type': 'application/json'
+};
+
 export const fetchProduct = async (produto: string) => {
   if (!produto) throw new Error('ID não informado');
-  const response = await fetch(`https://api.mercadolibre.com/items/${produto}`);
+  
+  const response = await fetch(`https://api.mercadolibre.com/items/${produto}`, { headers });
   const data = await response.json();
   return data;
 };
 
-export const fetchProductsList = async (item: string)=> {
+export const fetchProductsList = async (item: string) => {
   if (!item) throw new Error('Termo de busca não informado');
+  
   const response = await fetch(
     `https://api.mercadolibre.com/sites/MLB/search?q=${item}`,
+    { headers }
   );
-  const { results } = await response.json();
-  return results;
+  
+  const data = await response.json();
+ return data.results || []; 
 };
 
 export const fetchProductsByCategoriesList = async (category: string) => {
   if (!category) throw new Error('Termo de busca não informado');
+  
   const response = await fetch(
     `https://api.mercadolibre.com/sites/MLB/search?category=${category}`,
+    { headers }
   );
-  const { results } = await response.json();
-  return results;
+  
+  const data = await response.json();
+  return data.results || [];
 };
+
+
