c191a09635134ed8359a3154e98edac172599035
Bruno <riquebruno@yahoo.com.br>
2026-02-23 19:28:30 -0300

    fix: token error headers

--- src/utils/fetchFunctions.ts
+++ src/utils/fetchFunctions.ts
@@ -1,40 +1,42 @@
 const TOKEN = import.meta.env.VITE_ML_ACCESS_TOKEN;
+console.log(TOKEN);
 
 const headers = {
-  'Authorization': `Bearer ${TOKEN}`,
-  'Content-Type': 'application/json'
+  Authorization: `Bearer ${TOKEN}`,
+  'Content-Type': 'application/json',
 };
 
 export const fetchProduct = async (produto: string) => {
   if (!produto) throw new Error('ID não informado');
-  
-  const response = await fetch(`https://api.mercadolibre.com/items/${produto}`, { headers });
+
+  const response = await fetch(
+    `https://api.mercadolibre.com/items/${produto}`,
+    { headers },
+  );
   const data = await response.json();
   return data;
 };
 
 export const fetchProductsList = async (item: string) => {
   if (!item) throw new Error('Termo de busca não informado');
-  
+
   const response = await fetch(
     `https://api.mercadolibre.com/sites/MLB/search?q=${item}`,
-    { headers }
+    { headers },
   );
-  
-  const data = await response.json();
- return data.results || []; 
-};
 
-export const fetchProductsByCategoriesList = async (category: string) => {
-  if (!category) throw new Error('Termo de busca não informado');
-  
-  const response = await fetch(
-    `https://api.mercadolibre.com/sites/MLB/search?category=${category}`,
-    { headers }
-  );
-  
   const data = await response.json();
   return data.results || [];
 };
 
+export const fetchProductsByCategoriesList = async (category: string) => {
+  if (!category) throw new Error('Termo de busca não informado');
 
+  const response = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${category}`,
+    { headers },
+  );
+
+  const data = await response.json();
+  return data.results || [];
+};
